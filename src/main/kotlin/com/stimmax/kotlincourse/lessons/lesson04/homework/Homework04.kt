de8bf05e0b52c397c15da7ddc2968657b2e5dae4
Lilia.A <samohinalilia93@gmail.com>
2025-09-16 17:57:22 +0300

    домашнее задание 4

---
+++ src/main/kotlin/com/stimmax/kotlincourse/lessons/lesson04/homework/Homework04.kt
@@ -0,0 +1,71 @@
+package org.example.com.stimmax.kotlincourse.lessons.lesson04.homework
+
+class Homework04 {
+
+    val v1: Int = 42
+    val v2: Long = 98765432123456789L
+    val v3: Float = 23.45f
+    val v4: Double = 0.123456789
+    val v5: String = "Kotlin & Java"
+//    "false" - должно быть маленькими буквами
+//    val v6: Boolean = FALSE
+    val v7: Char = 'c'
+    val v8: Int = 500
+    val v9: Long = 4294967296L
+    val v10: Float = 18.0f
+    val v11: Double = -0.001
+    val v12: String = "OpenAI"
+    val v13: String = "true"
+    val v14: List<Int> = listOf(3, 14)
+    val v15: Char = '9'
+    val v16: Int = 2048
+    val v17: Long = 10000000000L
+    val v18: Set<String> = setOf("OpenAI", "Quantum Computing")
+    val v19: Float = 5.75f
+//    не должно быть кавычек
+//    val v20: Double = `1.414`
+    val v21: String = "Artificial Intelligence"
+//    несколько типов данных
+//    val v22: = arrayOf('x', "A")
+    val v23: String = "Android Studio"
+    val v24: Char = '@'
+    val v25: Int = 1024
+    val v26: Long = 1234567890123L
+    val v27: Float = 10.01f
+    val v28: Double = -273.15
+    val v29: String = "SpaceX"
+//    "false" - должно быть маленькими буквами
+//    val v30: Boolean = FALSE
+    val v31: Double = 0.007
+    val v32: Char = '🤯'
+    val v33 = mapOf("true" to 2, "false" to 34)
+//    не должно быть кавычек
+//    val v34: Int = ‘65535’
+    val v35: Long = 72057594037927935L
+    val v36: Float = 2.71828f
+    val v37: Double = 101.0101
+    val v38: String = "Quantum Computing"
+    val v39: Map<Int, String> = mapOf(2 to "true", 34 to "false")
+    val v40: Char = 'x'
+    val v41: Int = 314
+    val v42: Long = 123456789123456789L
+    val v43: Float = 6.626f
+//    "true" - должно быть маленькими буквами
+//    val v44: Boolean = TRUE
+
+//    ------------------------------------------------------------------
+
+    val s1: Char = 'B'
+    val s2: Int = 1
+    val s3: List<Double> = listOf(563.65, 1236.54)
+    val s4: Double = 45.987654321
+    val s5: Boolean = true
+    val s6: Int = 5
+    val s7: Int = 100500
+    val s8: Long = 8888888888888
+    val s9: Double = 2.54
+    val s10: Map<String, Double> = mapOf("Название 1" to 345765.65, "Название 2" to 567343.89)
+    val s11: String = "починилось само"
+    val s12: List<String> = listOf("тема 1", "тема 2")
+
+}
