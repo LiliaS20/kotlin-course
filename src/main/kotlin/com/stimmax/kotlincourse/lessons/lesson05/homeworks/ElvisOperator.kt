0110a4433d45d8ed3581ebb94fe0b3526048fe7c
Lilia.A <samohinalilia93@gmail.com>
2025-09-19 19:15:00 +0300

    домашнее задание 5-1

--- src/main/kotlin/com/stimmax/kotlincourse/lessons/lesson05/homeworks/ElvisOperator.kt
+++ src/main/kotlin/com/stimmax/kotlincourse/lessons/lesson05/homeworks/ElvisOperator.kt
@@ -3,8 +3,8 @@ package org.example.com.stimmax.kotlincourse.lessons.lesson05.homework
 fun main() {
 
 //    Задача 1
-    var initialSoundIntensity: Double = 5.0
-    var coefficient: Double? = null
+    val initialSoundIntensity: Double = 5.0
+    val coefficient: Double? = null
     val baseCoefficient = 0.5
     val resultIntensity = initialSoundIntensity * (coefficient?: baseCoefficient)
     println(resultIntensity)
@@ -12,14 +12,14 @@ fun main() {
 
 //    Задача 2
     val insurance: Double = 0.005
-    var defaultCost: Double = 50.0
-    var cost: Double? = null
-    var costDelivery = insurance * (cost ?: defaultCost)
-    var sumDelivery = costDelivery + (cost ?: defaultCost)
+    val defaultCost: Double = 50.0
+    val cost: Double? = null
+    val costDelivery = insurance * (cost ?: defaultCost)
+    val sumDelivery = costDelivery + (cost ?: defaultCost)
     println(sumDelivery)
 
 //    Задача 3
-    var pressure: String? = null
-    var infoPressure = pressure ?: "показания отсутствуют"
+    val pressure: String? = null
+    val infoPressure = pressure ?: "показания отсутствуют"
     println(infoPressure)
 }
