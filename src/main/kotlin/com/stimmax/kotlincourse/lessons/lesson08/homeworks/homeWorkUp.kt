953b7c055316333e2402b45919072a22dc03fd1a
Lilia.A <samohinalilia93@gmail.com>
2025-10-03 14:00:30 +0300

    домашнее задание 8 с правками по допзаданию

--- src/main/kotlin/com/stimmax/kotlincourse/lessons/lesson08/homeworks/homeWorkUp.kt
+++ src/main/kotlin/com/stimmax/kotlincourse/lessons/lesson08/homeworks/homeWorkUp.kt
@@ -1,13 +1,16 @@
 package org.example.com.stimmax.kotlincourse.lessons.lesson08.homeworks
 
 fun main() {
-    example8(260, 500)
+    example8(34, 55)
 }
 
+//Напишите метод, который преобразует строку из нескольких слов в строку, где каждое слово начинается с
+// заглавной буквы, а все остальные - строчные. Используй перебор, анализ символов и замену букв на заглавную
+// с помощью метода uppercase() для конкретной буквы.
 fun example7(arg: String) {
     lateinit var letter: String
     var result: String = arg.elementAt(0).uppercase()
-    for (i in 1..arg.length - 1) {
+    for (i in 1 until arg.length) {
         if (arg[i - 1] == ' ') {
             letter = arg.elementAt(i).uppercase()
             result = "$result$letter"
@@ -18,6 +21,11 @@ fun example7(arg: String) {
     println(result)
 }
 
+// Напишите шифратор/дешифратор для строки. Шифровка производится путём замены двух соседних букв между собой:
+// Kotlin шифруется в oKltni. Дешифровка выполняется аналогично.
+//Если длина строки - нечётная, в конец добавляется символ пробела до начала шифрования. Таким образом все шифрованные
+// сообщения будут с чётной длинной. Должно получиться два публичных метода: encrypt() и decrypt() которые
+// принимают строку и печатают результат в консоль.
 fun encrypt(arg: String) {
     lateinit var str: String
     var result = ""
@@ -28,7 +36,7 @@ fun encrypt(arg: String) {
         str = arg
     }
 
-    for (i in 0..str.length - 1 step 2) {
+    for (i in 0 until str.length step 2) {
         result = "$result${str[i + 1]}${str[i]}"
     }
     println(result)
@@ -36,12 +44,16 @@ fun encrypt(arg: String) {
 
 fun decrypt(arg: String) {
     var result = ""
-    for (i in 0..arg.length - 1 step 2) {
+    for (i in 0 until arg.length step 2) {
         result = "$result${arg[i + 1]}${arg[i]}"
     }
     println(result.trim())
 }
 
+//Напишите функцию, которая принимает два числа и выводит таблицу умножения, у которой в заголовках столбцов и
+//строк находятся перемножаемые числа, а в перекрестии заголовка и столбца - результат перемножения. Важно:
+// каждый столбец должен быть выровнен по правому краю с помощью шаблона с форматированием строк.
+// Размер форматирования каждой строки нужно вычислять динамически для каждого столбца.
 fun example8(arg1: Int, arg2: Int) {
     val countMax = (arg1 * arg2).toString().length + 2 // получаем максимальную длину числа при умножении
     val argMax = (if (arg1 > arg2) arg1 else arg2).toString().length // получаем максимальную длину аргумента
